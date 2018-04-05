621478e18b58e2408c0742bed7eebd36bb132232
Drew Palazzari <drewpalazzari@hotmail.com>
2018-04-04 21:47:27 -0600

    Update routes

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,7 @@ var HaikuModel = require('./models/haiku');
 
 module.exports = function(app) {
 
-    app.get('/api/haikus', function(req, res) {
+    app.get('/api/v1/haikus', function(req, res) {
         HaikuModel.find(function(err, response) {
             if (err) {
                 console.log(err);
@@ -14,7 +14,7 @@ module.exports = function(app) {
         });
     });
 
-    app.get('/api/haiku/:title', function(req, res) {
+    app.get('/api/v1/haiku/:title', function(req, res) {
         var title = req.params.title;
         HaikuModel.find({ Title: title }, function(err, response) {
             if (err) {
@@ -26,7 +26,7 @@ module.exports = function(app) {
         });
     });
 
-    app.get('/api/haiku/random', function(req, res) {
+    app.get('/api/v1/haiku/random', function(req, res) {
         HaikuModel.count().exec(function (err, count) {
             var random = Math.floor(Math.random() * count);
             HaikuModel.findOne().skip(random).exec(
@@ -40,7 +40,7 @@ module.exports = function(app) {
               });
           });
     });
-    app.post('/api/haiku', function(req, res) {
+    app.post('/api/v1/haiku', function(req, res) {
         var data = req.body;
         data.CreatedDate = new Date();
         HaikuModel.create(data, function(err, response) {
@@ -53,7 +53,7 @@ module.exports = function(app) {
         });
     });
 
-    app.put('/api/haiku', function(req, res) {
+    app.put('/api/v1/haiku', function(req, res) {
         var data = req.body;
         data.UpdatedDate = new Date();
         HaikuModel.update(data, function(err, response) {
@@ -66,7 +66,7 @@ module.exports = function(app) {
         });
     });
 
-    app.delete('/api/haiku/:title', function(req, res) {
+    app.delete('/api/v1/haiku/:title', function(req, res) {
         var title = req.params.title;
         HaikuModel.deleteOne({ Title: title }, function(err, response) {
             if (err) {
@@ -78,7 +78,7 @@ module.exports = function(app) {
         });
     });
 
-    app.get('/whatisthis', function(req, res) {
+    app.get('/', function(req, res) {
         res.sendfile('./views/index.html');
     });
 };
